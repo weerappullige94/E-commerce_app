791642c61c0431e8f90fb362328b8d6a3593ff4b
Tharanga Weerappullige <tharangasadamali94@gmail.com>
2019-10-17 14:16:29 +0530

    completed firebase login and signup

--- app/src/main/java/com/example/ecommerce/LoginActivity.java
+++ app/src/main/java/com/example/ecommerce/LoginActivity.java
@@ -3,8 +3,13 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button LoginButton;
+    private EditText InputNumber, InputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
